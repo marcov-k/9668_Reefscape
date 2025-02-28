b983672e510c1cfb8834bdeed7118dfab102adc2
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2025-02-28 09:50:08 -0500

    Update CAN IDs
    I reset all the SparkMax CAD Ids.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,14 +46,15 @@ public final class Constants {
 
     // SPARK MAX CAN IDs
     public static final int kFrontLeftDrivingCanId = 1;
-    public static final int kRearLeftDrivingCanId = 2;
-    public static final int kFrontRightDrivingCanId = 3;
-    public static final int kRearRightDrivingCanId = 8;
+    public static final int kFrontRightDrivingCanId = 2;
+    public static final int kRearLeftDrivingCanId = 3;    
+    public static final int kRearRightDrivingCanId = 4;
 
-    public static final int kFrontLeftTurningCanId = 7;
-    public static final int kRearLeftTurningCanId = 4;
-    public static final int kFrontRightTurningCanId = 9;
-    public static final int kRearRightTurningCanId = 5;
+    public static final int kFrontLeftTurningCanId = 5;    
+    public static final int kFrontRightTurningCanId = 6;
+    public static final int kRearLeftTurningCanId = 8;
+    public static final int kRearRightTurningCanId = 7;
+    
     
     public static final double kUnitstoFeet = 4.2;
   }
